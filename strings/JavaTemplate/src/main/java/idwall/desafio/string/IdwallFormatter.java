beea9ee33cb0686a9987519ad4b7f7aa086a9f1d
Yuri Cordeiro <yuricordeiroc@hotmail.com>
2018-02-28 13:53:34 -0300

    First Commit

--- strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
+++ strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
@@ -5,14 +5,42 @@ package idwall.desafio.string;
  */
 public class IdwallFormatter extends StringFormatter {
 
-    /**
-     * Should format as described in the challenge
-     *
-     * @param text
-     * @return
-     */
-    @Override
-    public String format(String text) {
-        throw new UnsupportedOperationException();
-    }
+	/**
+	 * Should format as described in the challenge
+	 *
+	 * @param text
+	 * @return
+	 */
+	@Override
+	public String format(String text, Integer limit) {
+		// throw new UnsupportedOperationException();
+		return splitText(text, limit);
+	}
+
+	public String splitText(String text, int limit) {
+		StringBuilder sb = new StringBuilder();
+		char[] textArray = text.toCharArray();
+		boolean endOfString = false;
+		int start = 0;
+		int end = start;
+
+		while (start < textArray.length) {
+			int charCount = 0;
+			int lastSpace = 0;
+			while (charCount <= limit) {
+				if (textArray[charCount + start] == ' ' || textArray[charCount + start] == '\n') {
+					lastSpace = charCount;
+				}
+				charCount++;
+				if (charCount + start == text.length()) {
+					endOfString = true;
+					break;
+				}
+			}
+			end = endOfString ? text.length() : (lastSpace > 0) ? lastSpace + start : charCount + start;
+			sb.append(text.substring(start, end) + "\n");
+			start = end + 1;
+		}
+		return sb.toString();
+	}
 }
---
+++ strings/JavaTemplate/src/test/java/idwall/desafio/string/IdWallFormatterTest.java
@@ -0,0 +1,25 @@
+package idwall.desafio.string;
+
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+
+public class IdWallFormatterTest {
+
+//	private static final String DEFAULT_INPUT_TEXT = "In the beginning God created the heavens and the earth. Now the earth was formless and empty, darkness was over the surface of the deep, and the Spirit of God was hovering over the waters.\n"
+//			+ "\n"
+//			+ "And God said, \"Let there be light,\" and there was light. God saw that the light was good, and he separated the light from the darkness. God called the light \"day,\" and the darkness he called \"night.\" And there was evening, and there was morning - the first day.";
+//
+//	@Test
+//	public void testFormat() {
+//		IdwallFormatter iwft = new IdwallFormatter();
+//		assertEquals("In the beginning God created the heavens\r\n" + "and the earth. Now the earth was\r\n"
+//				+ "formless and empty, darkness was over\r\n" + "the surface of the deep, and the Spirit\r\n"
+//				+ "of God was hovering over the waters.\r\n" + "\r\n" + "And God said, \"Let there be light,\" and\r\n"
+//				+ "there was light. God saw that the light\r\n" + "was good, and he separated the light\r\n"
+//				+ "from the darkness. God called the light\r\n" + "\"day,\" and the darkness he called\r\n"
+//				+ "\"night.\" And there was evening, and\r\n" + "there was morning - the first day.",
+//				iwft.format(DEFAULT_INPUT_TEXT, 40));
+//	}
+
+}
